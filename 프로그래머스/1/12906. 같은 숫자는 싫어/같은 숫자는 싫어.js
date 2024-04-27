fe6c57d18a3baea3c26c0a29d70f3247c25f1da5
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-04-27 21:56:26 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 31.27 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,12 @@
+function solution(arr){
+    const stack = []
+    stack.push(arr[0])
+    
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i - 1] !== arr[i]) {
+            stack.push(arr[i])
+        }
+    }
+    
+    return stack;
+}
