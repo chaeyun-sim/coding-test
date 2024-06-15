c7ca577248cc26ab980e0021f92191aa1c7b1677
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-06-15 13:26:05 +0900

    [goormlevel] Title: 정수의 길이, Time: 0.05 ms, Memory: 31.27 MB, Difficulty: 1 -BaekjoonHub

---
+++ goormlevel/173089/1. 정수의 길이/정수의 길이.js
@@ -0,0 +1,15 @@
+// Run by Node.js
+const readline = require('readline');
+
+(async () => {
+	let rl = readline.createInterface({ input: process.stdin });
+	const input = []
+	
+	for await (const line of rl) {
+		input.push(line)
+	}
+	
+	console.log(input[0].length)
+	
+	process.exit();
+})();
