051d34da8a342a42f649c8ebfe06b2224349b71a
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-09-06 22:34:19 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.32 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
@@ -0,0 +1,20 @@
+function timeToSeconds(time) {
+    const [minute, second] = time.split(":").map(Number);
+    return minute * 60 + second;
+}
+
+function solution(video_len, pos, op_start, op_end, commands) {
+    let current = timeToSeconds(pos);
+    const videoEnd = timeToSeconds(video_len);
+    const openingStart = timeToSeconds(op_start);
+    const openingEnd = timeToSeconds(op_end);
+
+    commands.forEach(cmd => {
+        if (openingStart <= current && current <= openingEnd) current = openingEnd;
+        if (cmd === "prev") current = Math.max(0, current - 10);
+        if (cmd === "next") current = Math.min(videoEnd, current + 10);
+    });
+    
+    if (openingStart <= current && current <= openingEnd) current = openingEnd;
+    return `${String(Math.floor(current / 60)).padStart(2, '0')}:${String(current % 60).padStart(2, '0')}`
+}
