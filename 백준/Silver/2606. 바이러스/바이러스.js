237986c7b83e1fdb6c97382b4abf7778d4aab1fc
chaeyun-sim <111689342+chaeyun-sim@users.noreply.github.com>
2025-02-08 13:37:04 +0900

    [Silver III] Title: 바이러스, Time: 92 ms, Memory: 9420 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.js
+++ 백준/Silver/2606. 바이러스/바이러스.js
@@ -1,27 +1,32 @@
-const fs = require('fs');
+const fs = require('fs')
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 
-let [n, k, ...arr] = input
-n = Number(n)
-arr = arr.map(el => el.split(' ').map(Number))
-let graph = new Array(n + 1).fill().map((_, i) => [])
-for (let i = 0; i < k; i++) {
-    let [x, y] = arr[i]
-    graph[x].push(y)
-    if (y < k + 1) graph[y].push(x)   
-}
-let visited = Array(n).fill(false);
+const n = Number(input[0])
+const T = Number(input[1])
+const arr = input.slice(2).map(el => el.split(' ').map(Number))
 
-let cnt = 0
+const graph = Array.from({ length: n + 1}, () => [])
+const visited = new Array(n + 1).fill(false)
+
+arr.forEach(([a, b]) => {
+    graph[a].push(b)
+    graph[b].push(a)
+})
+
+const result = []
+
 function dfs(graph, v, visited) {
     visited[v] = true
-    if (v !== 1) cnt++
-    for (i of graph[v]) {
-        if (!visited[i]) {
-            dfs(graph, i, visited)
+    result.push(v)
+
+    for (let i = 0; i < graph[v].length; i++) {
+        const num = graph[v][i]
+        if (!visited[num]) {
+            dfs(graph, num, visited)
         }
     }
 }
 
 dfs(graph, 1, visited)
-console.log(cnt)
+
+console.log(result.length - 1)
