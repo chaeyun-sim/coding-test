f8c4e3b7541ede317ce31030b1fc0dbaaf162823
chaeyun-sim <111689342+chaeyun-sim@users.noreply.github.com>
2025-03-06 13:32:40 +0900

    [level 2] Title: 할인 행사, Time: 23.81 ms, Memory: 43.1 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,27 +1,41 @@
-function checkEqual(origin, result) {
-    const a = Array.from(origin).sort((a, b) => a[0].localeCompare(b[0]))
-    const b = Array.from(result).sort((a, b) => a[0].localeCompare(b[0]))
-    
-    for (let i = 0; i < a.length; i++) {
-        if (a[i][0] !== b[i][0] || a[i][1] !== b[i][1]) {
-            return false
-        }
+function checkEqual(needToBuy, map) {
+    for (const [key, value] of needToBuy.entries()) {
+        if (map.get(key) !== value) return false;
     }
     return true
 }
 
 function solution(want, number, discount) {
-    const map = new Map(want.map((el, i) => [el, number[i]]))
-    let cnt = 0
+    const needToBuy = new Map(want.map((el, i) => [el, number[i]]))
+    const n = 10
+    const map = new Map()
     
-    for (let i = 0; i < discount.length; i++) {
-        const resultMap = new Map()
-        const arr = discount.slice(i, i + 10)
-        if (arr.length === 10) {
-            arr.forEach(item => resultMap.set(item, (resultMap.get(item) || 0) + 1))
-            if (checkEqual(map, resultMap)) cnt++
-        }
+    
+    for (let i = 0; i < n; i++) {
+        map.set(discount[i], (map.get(discount[i]) || 0) + 1)
     }
-    return cnt   
-}
     
+    let count = +checkEqual(needToBuy, map)
+    
+    let left = 0
+    let right = n
+    // 0 ~ 9, 1 ~ 10
+    while (right < discount.length) {
+        const removedItem = discount[left]
+        const addedItem = discount[right]
+        
+        if (map.get(removedItem) === 1) {
+            map.delete(removedItem);
+        } else {
+            map.set(removedItem, map.get(removedItem) - 1);
+        }
+        map.set(addedItem, (map.get(addedItem) || 0) + 1)
+        
+        count += (+checkEqual(needToBuy, map))
+        
+        left++
+        right++
+    }
+    
+    return count
+}
