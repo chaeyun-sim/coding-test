d13cce94556b8471d273984c32d349cb69243c5c
chaeyun-sim <111689342+chaeyun-sim@users.noreply.github.com>
2025-03-10 14:39:01 +0900

    [level 2] Title: 기능개발, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -0,0 +1,25 @@
+function solution(progresses, speeds) {
+    const LIMIT = 100
+    const arr = []
+    
+    for (let i = 0; i < progresses.length; i++) {
+        const p = progresses[i]
+        arr.push(Math.ceil((LIMIT - p) / speeds[i]))
+    }
+    
+    let cnt = 0
+    const result = []
+    let max = arr[0]
+    
+    for (const item of arr) {
+        if (item > max) {
+            result.push(cnt)
+            cnt = 1
+            max = item
+        } else {
+            cnt++
+        }
+    }
+    
+    return [...result, cnt]
+}
