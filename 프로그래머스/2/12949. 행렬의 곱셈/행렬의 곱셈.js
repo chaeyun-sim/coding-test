94c894cc0b55f9c92a45a6bb78be1680317e263e
chaeyun-sim <111689342+chaeyun-sim@users.noreply.github.com>
2025-03-11 18:29:43 +0900

    [level 2] Title: 행렬의 곱셈, Time: 9.88 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -1,15 +1,19 @@
 function solution(arr1, arr2) {
-    const len1 = arr1.length
-    const len2 = arr2[0].length
-    const result = new Array(len1).fill([]).map(() => new Array(len2).fill(0))
+    const h = arr1.length;
+    const w = arr2[0].length;
+    const mid = arr2.length;
     
-    for (let i = 0; i < len1; i++) {
-        for (let k = 0; k < arr2.length; k++) {
-            for (let j = 0; j < len2; j++) {
-                result[i][j] += arr1[i][k] * arr2[k][j];
+    const result = Array.from({ length: h }, () => Array(w).fill(0));
+    
+    for (let i = 0; i < h; i++) {
+        for (let j = 0; j < w; j++) {
+            let sum = 0;
+            for (let k = 0; k < mid; k++) {
+                sum += arr1[i][k] * arr2[k][j];
             }
+            result[i][j] = sum;
         }
     }
-
+    
     return result;
 }
