d375d453cd4b01a36d120f23032bf375ccb37d87
chaeyun-sim <111689342+chaeyun-sim@users.noreply.github.com>
2025-03-31 10:56:16 +0900

    [Silver III] Title: 소수 구하기, Time: 2892 ms, Memory: 17032 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+
+function isPrime(n) {
+  if (n < 2) return false;
+  if (n === 2) return true;
+  if (n % 2 === 0) return false;
+
+  for (let i = 3; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) return false;
+  }
+
+  return true;
+}
+
+for (let i = n; i <= m; i++) {
+  if (isPrime(i)) console.log(i);
+}
