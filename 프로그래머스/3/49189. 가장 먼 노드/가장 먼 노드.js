2be996cb0d4ae234efdca764834be266dff4e92b
chaeyun-sim <111689342+chaeyun-sim@users.noreply.github.com>
2025-04-03 20:06:12 +0900

    [level 3] Title: 가장 먼 노드, Time: 50.08 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -7,21 +7,20 @@ function solution(n, edge) {
     })
     
     const distance = Array.from({ length: n + 1}, () => 0)
-    const visited = new Set()
+    const visited = Array.from({ length: n + 1}, () => false)
         
     function bfs(v) {
         const queue = [v]
-        visited.add(v)
+        visited[v] = true
         
         while (queue.length > 0) {
             const current = queue.shift()
-            console.log(current)
             
             graph[current].forEach(n => {
-                if (!visited.has(n)) {
+                if (!visited[n]) {
                     queue.push(n)
                     distance[n] = distance[current] + 1
-                    visited.add(n)
+                    visited[n] = true
                 }
             })
         }
