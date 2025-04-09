0b61091e6d3e90c41e79098b178d39645853c6c6
chaeyun-sim <111689342+chaeyun-sim@users.noreply.github.com>
2025-04-09 17:20:52 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 96 ms, Memory: 9500 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.js
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.js
@@ -5,15 +5,8 @@ const n = Number(input[0]);
 const target = input[1];
 
 const [prefix, suffix] = target.split('*');
+const regex = new RegExp(`^${prefix}.*${suffix}$`);
 
 for (let i = 2; i < 2 + n; i++) {
-  const str = input[i];
-
-  if (str.length < target.length - 1) {
-    console.log('NE');
-  } else if (str.startsWith(prefix) && str.endsWith(suffix)) {
-    console.log('DA');
-  } else {
-    console.log('NE');
-  }
+  console.log(regex.test(input[i]) ? 'DA' : 'NE')
 }
