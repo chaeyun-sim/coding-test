913f9ea87c50f0b41978f811eeef78fc21289e67
chaeyun-sim <111689342+chaeyun-sim@users.noreply.github.com>
2025-04-15 11:29:44 +0900

    [Silver I] Title: 포도주 시식, Time: 156 ms, Memory: 10392 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.js
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.js
@@ -1,15 +1,15 @@
-const fs = require('fs')
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const [T, ...arr] = input.map(Number)
-const dp = new Array(T + 1).fill(0)
+const n = Number(input[0]);
+const arr = input.slice(1).map(Number);
+const dp = Array.from({ length: n + 1 }, () => 0);
 
-dp[1] = arr[0]
-dp[2] = arr[0] + arr[1]
+dp[1] = arr[0];
+dp[2] = Math.max(arr[1], arr[0] + arr[1]);
 
-for (let i = 3; i <= T; i++) {
-    dp[i] = Math.max(dp[i - 1], dp[i - 2] + arr[i - 1], dp[i - 3] + arr[i - 2] + arr[i - 1])
+for (let i = 3; i <= n; i++) {
+  dp[i] = Math.max(dp[i - 1], dp[i - 2] + arr[i - 1], dp[i - 3] + arr[i - 1] + arr[i - 2]);
 }
 
-
-console.log(dp[T])
+console.log(dp[n]);
