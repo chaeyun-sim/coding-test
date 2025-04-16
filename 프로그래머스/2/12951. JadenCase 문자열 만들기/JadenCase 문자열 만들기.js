64c3c606f8ec6cc038702fcb32fe3d6b70ccdfe7
chaeyun-sim <111689342+chaeyun-sim@users.noreply.github.com>
2025-04-17 00:02:21 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,13 +1,13 @@
 function solution(s) {
-    let result = ''
+    const result = []
     
     for (let i = 0; i < s.length; i++) {
         if (i === 0 || s[i - 1] === ' ') {
-            result += s[i].toUpperCase()
+            result.push(s[i].toUpperCase())
         } else {
-            result += s[i].toLowerCase()
+            result.push(s[i].toLowerCase())
         }
     }
     
-    return result
+    return result.join('')
 }
