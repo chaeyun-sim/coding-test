d7e733de05132e9d6e1a59f43b88577b25ceab4e
chaeyun-sim <111689342+chaeyun-sim@users.noreply.github.com>
2025-04-21 11:12:31 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.32 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,27 +1,25 @@
 function solution(new_id) {
-    let str = new_id.toLowerCase()
+  let str = new_id
+    .toLowerCase()
+    .replaceAll(/[^a-z0-9\-_.]/g, '')
+    .replaceAll(/\.{2,}/g, '.')
+    .replace(/^\.|\.$/g, '');
+  
+  if (str.length === 0) str = 'a'
     
-    str = str.replaceAll(/[^a-z0-9\-_.]/g, '');
-    
-    str = str.replaceAll(/\.{2,}/g, '.');
-    
-    str = str.replace(/^\.|\.$/g, '');
-    
-    if (str.length === 0) str = 'a'
-    
-    if (str.length >= 16) {
-        str = str.slice(0, 15)
-        if (str[str.length - 1] === '.') {
-            str = str.slice(0, 14)
-        }
+  if (str.length >= 16) {
+    str = str.slice(0, 15)
+    if (str[str.length - 1] === '.') {
+      str = str.slice(0, 14)
     }
+  }
     
-    if (str.length <= 2) {
-        const last = str[str.length - 1]
-        for (let i = 0; i <= (3 - str.length); i++) {
-            str += last
-        }
+  if (str.length <= 2) {
+    const last = str[str.length - 1]
+    for (let i = 0; i <= (3 - str.length); i++) {
+      str += last
     }
+  }
     
-    return str
+  return str
 }
